042ca4008bc5aeff7926f1335ea9aa77c9f87853
Oscar Cumming <oscar.cumming21@gmail.com>
2025-11-01 16:29:49 +0000

    Basic spring uploaded

---
+++ src/test/code/syntax/load/balancer/ApplicationTests.java
@@ -0,0 +1,13 @@
+package code.syntax.load.balancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
